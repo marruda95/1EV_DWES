0bedd2e6ea0f284fa5fa274f94ef2c1dca28965f
marruda95 <arrudasanchezmery@gmail.com>
2021-12-04 13:08:18 +0100

    First commit

---
+++ AAMaria_Arruda/src/main/java/com/sanvalero/AAMaria_Arruda/AaMariaArrudaApplication.java
@@ -0,0 +1,13 @@
+package com.sanvalero.AAMaria_Arruda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaMariaArrudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaMariaArrudaApplication.class, args);
+	}
+
+}
---
+++ AAMaria_Arruda/src/test/java/com/sanvalero/AAMaria_Arruda/AaMariaArrudaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanvalero.AAMaria_Arruda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaMariaArrudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
